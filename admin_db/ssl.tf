49d9df67993f69079b4a35874a71a31ed1606808
Tim Williams <timw636@gmail.com>
2024-04-03 18:22:34 -0700

    changed secrets from data to resource

--- admin_db/ssl.tf
+++ admin_db/ssl.tf
@@ -8,9 +8,9 @@ resource "aws_acm_certificate" "cert" {
   }
 }
 
-data "aws_route53_zone" "zone" {
-  name         = var.domain_name
-  private_zone = false
+resource "aws_route53_zone" "zone" {
+  name = var.domain_name
+  # private_zone = false
 }
 
 resource "aws_route53_record" "record" {
@@ -27,7 +27,7 @@ resource "aws_route53_record" "record" {
   records         = [each.value.record]
   ttl             = 60
   type            = each.value.type
-  zone_id         = data.aws_route53_zone.zone.zone_id
+  zone_id         = aws_route53_zone.zone.zone_id
 }
 
 resource "aws_acm_certificate_validation" "cert" {
@@ -36,5 +36,5 @@ resource "aws_acm_certificate_validation" "cert" {
 }
 
 output "aws_route53_zone_id" {
-  value = data.aws_route53_zone.zone.zone_id
+  value = aws_route53_zone.zone.zone_id
 }
